2e993a1b7570b69f04e0e61e5b66546368176c23
Georgi Eftimov <jokatavr@gmail.com>
2015-06-09 00:00:06 +0300

    Match detail first thing.

---
+++ library/src/main/java/com/mobilesolutions/lolapi/models/MatchDetail.java
@@ -0,0 +1,40 @@
+package com.mobilesolutions.lolapi.models;
+
+import com.google.gson.annotations.Expose;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatchDetail {
+
+//    @Expose
+//    private int mapId;
+//    @Expose
+//    private long matchCreation;
+//    @Expose
+//    private long matchDuration;
+//    @Expose
+//    private long matchId;
+//    @Expose
+//    private String matchMode;
+//    @Expose
+//    private String matchType;
+//    @Expose
+//    private String matchVersion;
+//    @Expose
+//    private List<ParticipantIdentity> participantIdentities = new ArrayList<ParticipantIdentity>();
+//    @Expose
+//    private List<Participant> participants= new ArrayList<Participant>();
+//    @Expose
+//    private String platformId;
+//    @Expose
+//    private String queueType;
+//    @Expose
+//    private String region;
+//    @Expose
+//    private String season;
+//    @Expose
+//    private List<Team> teams= new ArrayList<Team>();
+//    @Expose
+//    private Timeline timeline;
+}
