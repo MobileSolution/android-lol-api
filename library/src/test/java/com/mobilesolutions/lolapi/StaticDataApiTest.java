5b0ca11b6ef6a2b8e48e0d25cbf96f8287a56640
martin.vitanov <martin.vitanovv@gmail.com>
2015-06-14 22:54:45 +0300

    Unit tests for Item APIs in Static data

--- library/src/test/java/com/mobilesolutions/lolapi/StaticDataApiTest.java
+++ library/src/test/java/com/mobilesolutions/lolapi/StaticDataApiTest.java
@@ -2,7 +2,10 @@ package com.mobilesolutions.lolapi;
 
 import com.mobilesolutions.lolapi.models.statics.ChampionDto;
 import com.mobilesolutions.lolapi.models.statics.ChampionListDto;
+import com.mobilesolutions.lolapi.models.statics.ItemDto;
+import com.mobilesolutions.lolapi.models.statics.ItemListDto;
 import com.mobilesolutions.lolapi.models.statics.enums.ChampDataEnum;
+import com.mobilesolutions.lolapi.models.statics.enums.ItemListEnum;
 import com.mobilesolutions.lolapi.utls.Constants;
 
 import junit.framework.Assert;
@@ -45,5 +48,39 @@ public class StaticDataApiTest extends BaseTest {
     public void testGetChampionByIdWithParams() {
         final ChampionDto championDataById = LolApi.getChampionDataById(23, Constants.DEFAULT_LOCALE, Constants.API_VERSION, ChampDataEnum.ALL);
         Assert.assertNotNull(championDataById);
+    }
+
+    @Test
+    public void testGetItemsList() {
+        final ItemListDto itemList = LolApi.getItemList();
+        Assert.assertTrue(itemList.getData().size() > 40);
+    }
+
+    @Test(expected = RetrofitError.class)
+    public void testGetItemsListError() {
+        final ItemListDto itemList = LolApi.getItemList("zzzz" ,"dfs", ItemListEnum.ALL);
+    }
+
+    @Test
+    public void testGetItemsListWithParams() {
+        final ItemListDto itemList = LolApi.getItemList(Constants.DEFAULT_LOCALE, Constants.API_VERSION, ItemListEnum.ALL);
+        Assert.assertTrue(itemList.getData().size() > 40);
+    }
+
+    @Test
+    public void testGetItemsById() {
+        final ItemDto itemById = LolApi.getItemById(3260);
+        Assert.assertNotNull(itemById);
+    }
+
+    @Test(expected = RetrofitError.class)
+    public void testGetItemsByIdError() {
+        final ItemDto itemById = LolApi.getItemById(3260 ,"fsa","sda",ItemListEnum.ALL);
+    }
+
+    @Test
+    public void testGetItemsByIdWithParams() {
+        final ItemDto itemById = LolApi.getItemById(3260, Constants.DEFAULT_LOCALE, Constants.API_VERSION, ItemListEnum.ALL);
+        Assert.assertNotNull(itemById);
     }
 }
